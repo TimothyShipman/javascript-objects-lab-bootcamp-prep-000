baec40655811c52fff1ef28d291b1ea82c09856b
Tim Shipman <bricklayertim@yahoo.com>
2017-11-01 19:45:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-    var recipes = { recipes, [prop]: "1", [prop2]: "2"};
+    var recipes = ({},recipes,{ prop: '1', prop2: '2' })
     return recipes;
   }
